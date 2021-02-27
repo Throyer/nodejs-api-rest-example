415b688b7542363f3822873428facf8690c5cbad
Renato Henrique <renato.henrique_98@hotmail.com>
2021-02-27 17:28:07 -0300

    Error format

---
+++ src/utils/format-errors.ts
@@ -0,0 +1,14 @@
+import { ValidationError } from 'class-validator';
+
+interface CustomError {
+    property: string;
+    errors: string[];
+}
+
+export const formatErrors = (errors: ValidationError[]): CustomError[] =>
+    errors && errors.length
+        ? errors.map(({ property, constraints }) => ({
+              property,
+              errors: Object.values(constraints),
+          }))
+        : [];
