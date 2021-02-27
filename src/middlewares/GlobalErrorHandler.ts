6fd376b733aec459b09562fec40b829d4bb9a300
Renato Henrique <renato.henrique_98@hotmail.com>
2021-02-27 17:27:32 -0300

    Better error handler

--- src/middlewares/GlobalErrorHandler.ts
+++ src/middlewares/GlobalErrorHandler.ts
@@ -1,34 +1,63 @@
-import { Response, Request, NextFunction } from 'express';
-
+import { ValidationError } from 'class-validator';
+import { NextFunction, Request, Response } from 'express';
 import {
+    ExpressErrorMiddlewareInterface,
+    HttpError,
     Middleware,
-    ExpressErrorMiddlewareInterface
-} from "routing-controllers";
-
-import HttpStatusError from "../errors/HttpStatusError";
+} from 'routing-controllers';
+import HttpStatusError from '../errors/HttpStatusError';
 import { HttpStatus } from '../shared/HttpStatus';
+import { formatErrors } from '../utils/format-errors';
 
-@Middleware({ type: "after" })
+interface Errors extends HttpStatusError {
+    errors?: ValidationError[];
+    httpCode?: number;
+    statusCode?: number;
+}
+
+@Middleware({ type: 'after' })
 export class GlobalErrorHandler implements ExpressErrorMiddlewareInterface {
+    error(
+        error: Errors,
+        request: Request,
+        respose: Response,
+        next: NextFunction,
+    ): Response {
 
-    error(error: Error, request: Request, respose: Response, next: NextFunction): Response {
+        if (error.statusCode) {
+            return respose.status(error.statusCode).json({
+                message: error.message,
+                status: error.httpCode,
+            });
+        }
+
+        if (error.httpCode === HttpStatus.BAD_REQUEST && error?.errors) {
+            return respose.status(error.httpCode).json({
+                errors: formatErrors(error.errors),
+                message: error.message,
+                status: error.httpCode,
+            });
+        }
+
+        if (error instanceof HttpError) {
+            return respose.status(error.httpCode).json({
+                message: error.message,
+                status: error.httpCode,
+            });
+        }
 
         if (error instanceof HttpStatusError) {
-            return respose
-                .status(error.status)
-                .json({
-                    message: error.message,
-                    staus: error.status
-                });
+            return respose.status(error.status).json({
+                message: error.message,
+                status: error.status,
+            });
         }
 
         console.error(error);
 
-        return respose
-            .status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .json({
-                message: "Internal server error",
-                staus: HttpStatus.INTERNAL_SERVER_ERROR
-            })
+        return respose.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+            message: 'Internal server error',
+            status: HttpStatus.INTERNAL_SERVER_ERROR,
+        });
     }
 }
