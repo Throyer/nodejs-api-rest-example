746a231833ca61ec722fa98bfe9310c907a5d88d
Renato Henrique <renato.henrique_98@hotmail.com>
2021-05-08 19:27:35 -0300

    Ops

--- src/models/quiz/index.ts
+++
@@ -1,5 +0,0 @@
-export { Quiz } from './Quiz';
-export { QuizResult } from './QuizResult';
-export { Question } from './Question';
-export { QuestionGroup } from './QuestionGroup';
-export { QuestionOption } from './QuestionOption';
